233ed4ba0264d1f2484e1bf84fb4af7b19394717
Dewa Putu Agie <agixdewa99@gmail.com>
2026-06-02 20:02:39 +0800

    fix: replace driver with dialect in drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./lib/schema.ts",
   out: "./drizzle",
-  driver: "turso",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
